b808d6f84283d895843d3880beda1fe52b241b49
FrancoTalles <francotallesextreme@gmail.com>
2023-03-07 19:13:56 -0400

    Fix: hashtag/:hashtag route

---
+++ src/controllers/hashtag.controllers.js
@@ -0,0 +1,22 @@
+import db from "../config/database.connection.js";
+
+export async function hashtagPosts(req, res) {
+  const hashtag = res.locals.hashtag;
+
+  try {
+    const posts_para_procurar = await db.query(
+      `
+        SELECT * FROM hashtag 
+        JOIN posts 
+        ON hashtag."postId" = posts.id 
+        WHERE hashtag.hashtag = $1;`,
+      [hashtag]
+    );
+
+    const posts = posts_para_procurar.rows;
+
+    res.status(200).send(posts);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- src/routes/hashtag.routes.js
+++ src/routes/hashtag.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { checkHashtag } from "../middlewares/hashtag.middleware.js";
 
 const hashtagRouter = Router();
 
-hashtagRouter.get("/hashtag/:hashtag");
+hashtagRouter.get("/hashtag/:hashtag", checkHashtag, );
 
 export default hashtagRouter;
