0a7f1c080ad148c67d2d277a50a793c4828fb1e1
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-03-10 16:41:05 -0300

    refactor: formato do objeto retornado para a rota /user/:id modificado

--- src/app.js
+++ src/app.js
@@ -18,7 +18,6 @@ app.use(cors());
 app.use(express.json());
 app.use([authRouter, postRouter, hashtagRouter, likesRouter, usersRouter]);
 
-
 const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
