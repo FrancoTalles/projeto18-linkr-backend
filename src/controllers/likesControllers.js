ef87e6921b947a445729ec81aacbaae712098137
rsankutty <r.sankutty@gmail.com>
2023-03-09 22:58:23 -0300

    correção na rota de likes

--- src/controllers/likesControllers.js
+++ src/controllers/likesControllers.js
@@ -12,7 +12,7 @@ export async function handleLikes(req, res) {
 
         if (query.rowCount === 0){
             await db.query(
-                `INSERT INTO likes(userId, PostId, liked) VALUES ($1,$2,$3);`,
+                `INSERT INTO likes("postId", "userId", liked) VALUES ($1,$2,$3);`,
                 [postId,userId,liked]
             )
         } else {
@@ -23,26 +23,6 @@ export async function handleLikes(req, res) {
         }
 
         return res.sendStatus(201);
-
-    } catch (err) {
-        return res.status(500).send(err.message)
-    }
-}
-
-export async function countLikes(req, res) {
-    const { postId } = req.params;
-    const userId = req.userId;
-
-    try {
-        const likesCounter = await db.query(
-            `
-            SELECT COUNT(*) 
-            FROM likes 
-            WHERE postid = $1 AND liked = true;`,
-            [postId]
-        )
-            
-        return res.status(200).send(likesCounter.rows[0])
 
     } catch (err) {
         return res.status(500).send(err.message)
--- src/routes/likesRoutes.js
+++ src/routes/likesRoutes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { handleLikes,countLikes } from "../controllers/likesControllers.js";
+import { handleLikes } from "../controllers/likesControllers.js";
 
 const likesRouter = Router();
 
 likesRouter.post("/likes", handleLikes);
-likesRouter.get("/postLikes/:postId", countLikes);
 
 export default likesRouter;
