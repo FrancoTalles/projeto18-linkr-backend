d2ff5400b280766ef66577189254086f47c52a3e
Alexandre Dresch <z3r0pc@protonmail.com>
2023-03-10 19:49:37 -0300

    feat: likes update

--- src/controllers/likesControllers.js
+++ src/controllers/likesControllers.js
@@ -1,24 +1,25 @@
 import db from "../config/databaseConnection.js";
 
 export async function handleLikes(req, res) {
-    const { postId, userId, liked } = req.body;
+    const { postId, liked } = req.body;
+    const { userIdValue } = res.locals;
 
     try {
         const query = await db.query(
             `
             SELECT * FROM likes WHERE "postId"=$1 AND "userId"=$2;`,
-            [postId,userId]
+            [postId, userIdValue]
         )
 
         if (query.rowCount === 0){
             await db.query(
                 `INSERT INTO likes("postId", "userId", liked) VALUES ($1,$2,$3);`,
-                [postId,userId,liked]
+                [postId,userIdValue,liked]
             )
         } else {
             await db.query(
                 `UPDATE likes SET liked = $1 WHERE "postId"=$2 AND "userId"=$3;`,
-                [liked,postId,userId]
+                [liked,postId,userIdValue]
                 )
         }
 
