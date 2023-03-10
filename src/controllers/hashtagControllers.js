c530246082a33e0b27a1f735bef7ccab4b02b2a7
FrancoTalles <francotallesextreme@gmail.com>
2023-03-10 03:01:49 -0400

    Saving for tomorrow

--- src/controllers/hashtagControllers.js
+++ src/controllers/hashtagControllers.js
@@ -1,16 +1,54 @@
 import db from "../config/databaseConnection.js";
 
-export async function hashtagPosts(req, res) {
+export async function hashtagPosts(_, res) {
   const hashtag = res.locals.hashtag;
+  const id = res.locals.userIdValue;
+  console.log("entrou aqui");
 
   try {
     const posts_para_procurar = await db.query(
-      `
-        SELECT * FROM hashtag 
-        JOIN posts 
-        ON hashtag."postId" = posts.id 
-        WHERE hashtag.hashtag = $1;`,
-      [hashtag]
+      `SELECT 
+      p."id" AS postId,
+      u."username" AS postAuthor,
+      u."pictureURL" AS authorPhoto,
+      p."description" AS postDescription,
+      p."link" AS postLink,
+      COUNT(l."id") AS likesCount,
+      (
+        SELECT 
+          json_agg(
+            json_build_object(
+              'name', u2."username"
+            )
+          )
+        FROM 
+          "likes" l2
+          INNER JOIN "users" u2 ON l2."userId" = u2."id"
+        WHERE 
+          l2."postId" = p."id" AND l2."liked" = true
+      ) AS whoLiked,
+      EXISTS(
+        SELECT 
+          1 
+        FROM 
+          "likes" l3
+        WHERE 
+          l3."userId" = $1 AND l3."postId" = p."id" AND l3."liked" = true
+      ) AS liked
+    FROM 
+      "posts" p
+      INNER JOIN "users" u ON p."userId" = u."id"
+      LEFT JOIN "likes" l ON p."id" = l."postId" AND l."liked" = true
+    WHERE 
+      p."description" LIKE $2
+    GROUP BY 
+      p."id",
+      u."username",
+      u."pictureURL"
+    ORDER BY 
+      p."createdAt" DESC
+    LIMIT 20;`,
+      [id, `%#${hashtag}%`]
     );
 
     const posts = posts_para_procurar.rows;
--- src/routes/hashtagRoutes.js
+++ src/routes/hashtagRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { hashtagPosts } from "../controllers/hashtagControllers.js";
 import { checkHashtag } from "../middlewares/hashtagMiddleware.js";
+import { tokenValidation } from "../middlewares/tokenValidation.js";
 
 const hashtagRouter = Router();
 
-hashtagRouter.get("/hashtag/:hashtag", checkHashtag, );
+hashtagRouter.get("/hashtag/:hashtag", tokenValidation, checkHashtag, hashtagPosts);
 
 export default hashtagRouter;
