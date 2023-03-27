3625d71c54f81a0439a8879aaa37c95c341e56fb
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-03-27 18:15:34 -0300

    refactor: Ajuste na contagem resharescount

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -15,51 +15,44 @@ export async function getAllPosts(id) {
   const result = await db.query(
     `
     SELECT 
-    p."id" AS postId,
-    p."userId" AS userId,
-    u."username" AS postAuthor,
-    u."pictureURL" AS authorPhoto,
-    p."description" AS postDescription,
-    p."link" AS postLink,
-    p."createdAt" AS createdAt,
-    COUNT(l."id") AS likesCount,
-    COUNT(r."id") AS resharesCount,
-    (
-      SELECT 
-        json_agg(
-          json_build_object(
-            'name', u2."username"
+      p."id" AS postId,
+      p."userId" AS userId,
+      u."username" AS postAuthor,
+      u."pictureURL" AS authorPhoto,
+      p."description" AS postDescription,
+      p."link" AS postLink,
+      p."createdAt" AS createdAt,
+      (SELECT COUNT(*) FROM "likes" WHERE "postId" = p."id" AND "liked" = true) AS likesCount,
+      (SELECT COUNT(*) FROM "reshare" WHERE "postId" = p."id") AS resharesCount,
+      (
+        SELECT 
+          json_agg(
+            json_build_object(
+              'name', u2."username"
+            )
           )
-        )
-      FROM 
-        "likes" l2
-        INNER JOIN "users" u2 ON l2."userId" = u2."id"
-      WHERE 
-        l2."postId" = p."id" AND l2."liked" = true
-    ) AS whoLiked,
-    EXISTS(
-      SELECT 
-        1 
-      FROM 
-        "likes" l3
-      WHERE 
-        l3."userId" = $1 AND l3."postId" = p."id" AND l3."liked" = true
-    ) AS liked
-    FROM 
-      "posts" p
-      INNER JOIN "users" u ON p."userId" = u."id"
-      LEFT JOIN "followers" f ON f."followedUser" = p."userId"
-      LEFT JOIN "likes" l ON p."id" = l."postId" AND l."liked" = true
-      LEFT JOIN "reshare" r ON p."id" = r."postId"
-    WHERE 
-      (p."userId" = $1 OR (f."userId" = $1 AND f."followedUser" = p."userId"))
-    GROUP BY 
-      p."id",
-      u."username",
-      u."pictureURL"
-    ORDER BY 
-      p."createdAt" DESC
-    LIMIT 20
+        FROM 
+          "likes" l2
+          INNER JOIN "users" u2 ON l2."userId" = u2."id"
+        WHERE 
+          l2."postId" = p."id" AND l2."liked" = true
+      ) AS whoLiked,
+      EXISTS(
+        SELECT 
+          1 
+        FROM 
+          "likes" l3
+        WHERE 
+          l3."userId" = $1 AND l3."postId" = p."id" AND l3."liked" = true
+      ) AS liked
+  FROM 
+    "posts" p
+    INNER JOIN "users" u ON p."userId" = u."id"
+  WHERE 
+    p."userId" = $1 OR p."userId" IN (SELECT "followedUser" FROM "followers" WHERE "userId" = $1)
+  ORDER BY 
+    p."createdAt" DESC
+  LIMIT 20
   `,
     [id]
   );
